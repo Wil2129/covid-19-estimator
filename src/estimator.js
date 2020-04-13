d57245768a61d0f896ed54035a065a027623b8fb
Teddy BAHA <michael_takeshi@yahoo.com>
2020-04-13 01:06:01 +0100

    Challenge 1, 2 and 3 completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,44 @@
-const covid19ImpactEstimator = (data) => data;
+const impactEstimator = (impact, data) => {
+  const { currentlyInfected } = impact;
+  const { timeToElapse, totalHospitalBeds, region } = data;
+  const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
+  const infectionsByRequestedTime = currentlyInfected * Math.floor(timeToElapse / 3);
+  const severeCasesByRequestedTime = Math.floor(
+    infectionsByRequestedTime * 0.15
+  );
+  const hospitalBedsByRequestedTime = Math.floor(
+    totalHospitalBeds * 0.35 - severeCasesByRequestedTime
+  );
+  const casesForICUByRequestedTime = infectionsByRequestedTime * 0.05;
+  const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
+  const dollarsInFlight = Math.floor(
+    (infectionsByRequestedTime
+      * avgDailyIncomeInUSD
+      * avgDailyIncomePopulation)
+      / timeToElapse
+  );
+  return {
+    ...impact,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
+
+const covid19ImpactEstimator = (data) => {
+  let impact;
+  let severeImpact;
+  impact = {};
+  severeImpact = {};
+  const { reportedCases } = data;
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+  impact = impactEstimator(impact, data);
+  severeImpact = impactEstimator(severeImpact, data);
+  return { data, impact, severeImpact };
+};
 
 export default covid19ImpactEstimator;
